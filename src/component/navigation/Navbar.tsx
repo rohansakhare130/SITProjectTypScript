e1715033013ece99490148ba8078856ac373e8ae
Rohan <rohansakhare130@gmail.com>
2025-03-13 13:45:27 +0530

    add logo

--- src/component/navigation/Navbar.tsx
+++ src/component/navigation/Navbar.tsx
@@ -1,6 +1,8 @@
+import Logo from '../assets/logo.png'
+
 const Navbar = () => {
     return (
-        <nav className="bg-gray-800">
+        <nav className="bg-amber-100">
             <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
                 <div className="relative flex h-16 items-center justify-between">
                     <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
@@ -20,16 +22,16 @@ const Navbar = () => {
                     </div>
                     <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                         <div className="flex shrink-0 items-center">
-                            <img src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500" alt="company" className="h-8 w-auto" />
+                            <img src={Logo} alt="company" className="h-11 w-20" />
 
                         </div>
                         <div className="hidden sm:ml-6 sm:block">
                             <div className="flex space-x-4">
 
                                 <a href="#" className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Home</a>
-                                <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
-                                <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Projects</a>
-                                <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
+                                <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-500 hover:bg-gray-700 hover:text-white">About</a>
+                                <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-500 hover:bg-gray-700 hover:text-white">Projects</a>
+                                <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-500 hover:bg-gray-700 hover:text-white">Contact</a>
                             </div>
                         </div>
                     </div>
